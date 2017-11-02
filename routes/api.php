26cc54789b43e71d1be292189b8c55f4e57ce99c
javierortegon <javierortegonmp@gmail.com>
2017-11-02 13:06:44 -0500

    agregando primeras funcionalidadesde de api RESTfull

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,27 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/v1/catalog','APICatalogController@index');
+
+Route::get('/v1/catalog/{id}','APICatalogController@show');
+
+Route::post('v1/catalog', function () {
+    // Zona de acceso restringido
+})->middleware('auth.basic');
+
+Route::put('/v1/catalog/{id}', function () {
+    // Zona de acceso restringido
+})->middleware('auth.basic');
+
+Route::delete('/v1/catalog/{id}', function () {
+    // Zona de acceso restringido
+})->middleware('auth.basic');
+
+Route::put('/v1/catalog/{id}/rent',function () {
+    // Zona de acceso restringido
+})->middleware('auth.basic');
+
+Route::put('/v1/catalog/{id}/return', function () {
+    // Zona de acceso restringido
+})->middleware('auth.basic');
